57cedf6f11f7d25256bd6dbc63d4636cd8eebb73
Grandmozer <120051473+Grandmozer@users.noreply.github.com>
2024-11-18 21:22:54 +0500

    fix: filter form

--- Modals/FilterModal/layout.php
+++ Modals/FilterModal/layout.php
@@ -7,7 +7,7 @@
             <label class="filter-modal__label" for="town">Город</label>
             <input type="text" name="town" placeholder="Москва">
             <button class="filter-modal__reset">Сбросить фильтры</button>
-        </form>>
+        </form>
         <button class="filter-modal__close">
             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
                 <path d="M12 36L36 12M12 12L36 36" stroke="#999999" stroke-width="2" stroke-linecap="round"
